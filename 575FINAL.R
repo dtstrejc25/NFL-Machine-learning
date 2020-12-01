c625083f7e346ea0699ba242f730c133e7d10493
dtstrejc25 <71232578+dtstrejc25@users.noreply.github.com>
2020-12-01 11:09:40 -0600

    Update 575FINAL.R

--- 575FINAL.R
+++ 575FINAL.R
@@ -301,9 +301,10 @@ trnIndex = sample(1:nr, size = round(TRG_PCT*nr), replace=FALSE)
 redchiTrn=chidataRED[trnIndex,]   #training data with the randomly selected row-indices
 redchiTst = chidataRED[-trnIndex, ]
 
-m2subset= select(redchiTrn, -c("home_team", "away_team", "sp", "field_goal_result", 
-                               "special", "punt_attempt", "pass_touchdown", "rush_touchdown", "posteam", "defteam",
+m2subset= select(redchiTrn, -c("home_team", "away_team", "sp", "field_goal_result", "game_date", "posteam_type"
+                               ,"special", "punt_attempt", "pass_touchdown", "rush_touchdown", "posteam", "defteam",
                                "touchdown", "fourth_down_failed", "fourth_down_converted", "punt_blocked")) 
+
 
 rpModel2=rpart(drive_ended_with_score ~ ., data=m2subset, method= "class", 
                parms = list(split = "information"), 
